399ddcdba21532fae687b0162bc07e790868c3b3
BirdMan84 <Dani_lokoo69@hotmail.com>
2024-04-30 21:03:53 +0200

    Primera Vesion

---
+++ src/refactorizacion/OperacionesAritmeticas.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package refactorizacion;
+
+/**
+ *
+ * @author Daniel
+ */
+public class OperacionesAritmeticas {
+    
+    
+     public static double Sumar(double a, double b) {
+        return a + b;
+    }
+
+    public static double Restar(double a, double b) {
+        return a - b;
+    }
+    
+    
+    
+}
